6b4141fe6ac8587fc513468b5e7e8238c7176c1d
a.a.laktionov <a.a.laktionov@in-delo.com>
2021-04-14 15:08:45 +0300

    cleanTime: { $sum: { $subtract: ["$a", "$b"] }

--- BACKEND.js
+++ BACKEND.js
@@ -1,20 +1,24 @@
 /**
  * Filter by date in mongo through aggregation 
  */
- const lastDays = moment(new Date()).subtract(1, 'day').toDate();
- const sessions = await Sessions.aggregate([
-    { $match: { startTime: { $gt: lastDays} } },
-    { $project: {'userAdName': 1, 'departament': 1, 'centralPath': 1, 'startTime': 1,
-                'endTime': 1,  'commonTime': 1,  'idlingTime': 1}},
+const lastDays = moment(new Date()).subtract(1, 'day').toDate();
+const sessions = await Sessions.aggregate([
+    { $match: { startTime: { $gt: lastDays } } },
+    {
+        $project: {
+            'userAdName': 1, 'departament': 1, 'centralPath': 1, 'startTime': 1,
+            'endTime': 1, 'commonTime': 1, 'idlingTime': 1
+        }
+    },
     //{ $group : { _id : "$departament", users: { $push: "$userAdName" } } },
-    { $sort: { 'startTime' : -1 } }
+    { $sort: { 'startTime': -1 } }
 ]);
 
 /**
  * Last item in a sorted collection by field + grouping
  */
 
- const lastSessions = await Sessions.aggregate(
+const lastSessions = await Sessions.aggregate(
     [
         { $sort: { startTime: 1, } },
         {
@@ -34,3 +38,24 @@
         }
     ]
 );
+
+/**
+ * Filtering, grouping, summation in an array of subtraction a and b
+ */
+
+const cleanTimeSessions = await Sessions.aggregate([
+    { $match: { startTime: { $gt: lastDays } } },
+    {
+        $group: {
+            _id: "$userAdName",
+            cleanTime: { $sum: { $subtract: ["$commonTime", "$idlingTime"] } }
+        }
+    },
+    {
+        $project:
+        {
+            name: '$_id',
+            cleanTime: '$cleanTime',
+        }
+    }
+]);
