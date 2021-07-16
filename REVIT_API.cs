5558ac5ff81451e10976e3dd48d614037a4dfed5
a.a.laktionov <a.a.laktionov@in-delo.com>
2021-07-16 14:41:42 +0300

    Creation DirectShape

--- REVIT_API.cs
+++ REVIT_API.cs
@@ -1,16 +1,21 @@
-    /**
-    * Disserialize json
-    */
-    public static T Deserialize<T>(string json) where T : new()
-        {
-            try
-            {
-                var obj = JsonConvert.DeserializeObject<T>(json, Settings);
-                return obj;
-            }
-            catch (Exception ex)
-            {
-                Log.WriteLog($"Error: Failed to deserialize json string into a valid object___{ex.Message}");
-                throw new Exception($"Error: Failed to deserialize json string into a valid object___{ex.Message}");
-            }
-        }
+/**
+* Disserialize json
+*/
+public static T Deserialize<T>(string json) where T : new()
+{
+    try
+    {
+        var obj = JsonConvert.DeserializeObject<T>(json, Settings);
+        return obj;
+    }
+    catch (Exception ex)
+    {
+        Log.WriteLog($"Error: Failed to deserialize json string into a valid object___{ex.Message}");
+        throw new Exception($"Error: Failed to deserialize json string into a valid object___{ex.Message}");
+    }
+}
+
+// Creation DirectShape
+DirectShape new_shape_element = DirectShape.CreateElement(linkDocumentMEP, new ElementId((int)BuiltInCategoryCableTrays));
+new_shape_element.SetShape(new List<GeometryObject>() { intersectSolid });
+new_shape_element.SetName("TEST");
