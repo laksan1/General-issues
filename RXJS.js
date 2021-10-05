d1ef5beab12b175265b228b57785f48f6c8e8819
a.a.laktionov <a.a.laktionov@in-delo.com>
2021-10-05 11:03:32 +0300

    Adde hotkeys for webstorm

--- RXJS.js
+++ RXJS.js
@@ -1,14 +1,14 @@
 
-    const users = [
-		{ name: 'Anna', age: 20 },
-		{ name: 'gdfg', age: 30 },
-		{ name: 'gfd', age: 40 },
-		{ name: 'gdfg', age: 55 },
-		{ name: 'fdgd', age: 70 },
-		{ name: 'bbbb', age: 80 },
-		{ name: 'bddrrr', age: 90 }
-	  ];
-	  interval(1000).pipe(
-		filter(v => users[v].age > 40),
-		map(v => users[v].name)
-	  ).subscribe(user => console.log('user', user));
+const users = [
+	{ name: 'Anna', age: 20 },
+	{ name: 'gdfg', age: 30 },
+	{ name: 'gfd', age: 40 },
+	{ name: 'gdfg', age: 55 },
+	{ name: 'fdgd', age: 70 },
+	{ name: 'bbbb', age: 80 },
+	{ name: 'bddrrr', age: 90 }
+];
+interval(1000).pipe(
+	filter(v => users[v].age > 40),
+	map(v => users[v].name)
+).subscribe(user => console.log('user', user));
