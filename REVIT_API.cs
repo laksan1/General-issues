9250a719c874411cb52d25c3d7660563340f4f58
a.a.laktionov <a.a.laktionov@in-delo.com>
2021-10-18 17:12:43 +0300

    added GetSolidFromBouindigBox

--- REVIT_API.cs
+++ REVIT_API.cs
@@ -3,19 +3,55 @@
 */
 public static T Deserialize<T>(string json) where T : new()
 {
-    try
-    {
-        var obj = JsonConvert.DeserializeObject<T>(json, Settings);
-        return obj;
-    }
-    catch (Exception ex)
-    {
-        Log.WriteLog($"Error: Failed to deserialize json string into a valid object___{ex.Message}");
-        throw new Exception($"Error: Failed to deserialize json string into a valid object___{ex.Message}");
-    }
+	try
+	{
+		var obj = JsonConvert.DeserializeObject<T>(json, Settings);
+		return obj;
+	}
+	catch (Exception ex)
+	{
+		Log.WriteLog($"Error: Failed to deserialize json string into a valid object___{ex.Message}");
+		throw new Exception($"Error: Failed to deserialize json string into a valid object___{ex.Message}");
+	}
 }
 
 // Creation DirectShape
 DirectShape new_shape_element = DirectShape.CreateElement(linkDocumentMEP, new ElementId((int)BuiltInCategoryCableTrays));
 new_shape_element.SetShape(new List<GeometryObject>() { intersectSolid });
 new_shape_element.SetName("TEST");
+
+static Solid GetSolidFromBouindigBox(BoundingBoxXYZ _boundingBoxXYZ)
+{
+	BoundingBoxXYZ bbox = _boundingBoxXYZ;
+
+	XYZ pt0 = new XYZ(bbox.Min.X, bbox.Min.Y, bbox.Min.Z);
+	XYZ pt1 = new XYZ(bbox.Max.X, bbox.Min.Y, bbox.Min.Z);
+	XYZ pt2 = new XYZ(bbox.Max.X, bbox.Max.Y, bbox.Min.Z);
+	XYZ pt3 = new XYZ(bbox.Min.X, bbox.Max.Y, bbox.Min.Z);
+
+	Line edge0 = Line.CreateBound(pt0, pt1);
+	Line edge1 = Line.CreateBound(pt1, pt2);
+	Line edge2 = Line.CreateBound(pt2, pt3);
+	Line edge3 = Line.CreateBound(pt3, pt0);
+
+	// Create loop, still in BBox coords
+
+	List<Curve> edges = new List<Curve>();
+	edges.Add(edge0);
+	edges.Add(edge1);
+	edges.Add(edge2);
+	edges.Add(edge3);
+
+	double height = bbox.Max.Z - bbox.Min.Z;
+
+	CurveLoop baseLoop = CurveLoop.Create(edges);
+
+	List<CurveLoop> loopList = new List<CurveLoop>();
+	loopList.Add(baseLoop);
+
+	Solid preTransformBox = GeometryCreationUtilities
+	  .CreateExtrusionGeometry(loopList, XYZ.BasisZ,
+		height);
+
+	return preTransformBox;
+}
