10bbdc389a79f652825cff1aee44e39d5cd28ca7
a.a.laktionov <a.a.laktionov@in-delo.com>
2022-03-21 15:14:30 +0300

    added nodemailer

---
+++ NodeMailer.js
@@ -0,0 +1,28 @@
+NOTIFICATION_WEB_EMAIL_PORT=25
+NOTIFICATION_WEB_EMAIL_HOST=
+NOTIFICATION_WEB_EMAIL_NAME=
+
+// Отправить сообщение на внешний адрес 
+const outlookBot = nodemailer.createTransport({
+    pool: true,
+    host: mail.in-delo.com,
+    port: 587,
+    secure: false,
+    auth: {
+        user: nika.motion@in-delo.com,
+        pass: 111111,
+    },
+    tls: {
+       rejectUnauthorized: false,
+    }
+});
+
+// Отправить сообщение на локальный адрес 
+const outlookBot = nodemailer.createTransport({
+    host: mail.in-delo.com,
+    port: 25,
+	secureConnection: false,
+    tls: {
+		ciphers:'SSLv3'
+    }
+});
