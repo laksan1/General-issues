0c86eba3cfbaaf76188607e246a54d02a3c56853
a.a.laktionov <a.a.laktionov@in-delo.com>
2022-03-22 08:34:04 +0300

    // Отправить сообщение на локальный

--- NodeMailer.js
+++ NodeMailer.js
@@ -1,28 +1,37 @@
-NOTIFICATION_WEB_EMAIL_PORT=25
-NOTIFICATION_WEB_EMAIL_HOST=
-NOTIFICATION_WEB_EMAIL_NAME=
+NOTIFICATION_WEB_EMAIL_PORT = 25
+NOTIFICATION_WEB_EMAIL_HOST =
+	NOTIFICATION_WEB_EMAIL_NAME =
 
 // Отправить сообщение на внешний адрес 
 const outlookBot = nodemailer.createTransport({
-    pool: true,
-    host: mail.in-delo.com,
-    port: 587,
-    secure: false,
-    auth: {
-        user: nika.motion@in-delo.com,
-        pass: 111111,
+		pool: true,
+		host: mail.in - delo.com,
+		port: 587,
+		secure: false,
+		auth: {
+			user: nika.motion@in -delo.com,
+				pass: 111111,
     },
-    tls: {
-       rejectUnauthorized: false,
+tls: {
+	rejectUnauthorized: false,
     }
 });
 
 // Отправить сообщение на локальный адрес 
 const outlookBot = nodemailer.createTransport({
-    host: mail.in-delo.com,
-    port: 25,
+	host: mail.in - delo.com,
+	port: 25,
 	secureConnection: false,
-    tls: {
-		ciphers:'SSLv3'
-    }
+	tls: {
+		ciphers: 'SSLv3'
+	}
 });
+
+// 1. Ошибка, если  secure: true или при ciphers: 'SSLv3'
+{
+	library: 'SSL routines',
+		function: 'ssl3_get_record',
+		reason: 'wrong version number',
+			code: 'ESOCKET',
+				command: 'CONN'
+}
