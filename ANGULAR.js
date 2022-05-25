60d35e7b7d15cc53e88a71f01fa6baf83ef0be08
a.a.laktionov <a.a.laktionov@in-delo.com>
2022-05-25 15:10:03 +0300

    added this.ngZone.runOutsideAngular(()

--- ANGULAR.js
+++ ANGULAR.js
@@ -1,1 +1,13 @@
 ng g m modules/bim-analytics --routing --force // Добавить модуль с роутом
+
+// Отключение ngZone
+@Injectable()
+export class SomeService {
+	constructor(private ngZone: NgZone){
+		this.ngZone.runOutsideAngular(() => {
+			interval(1).subscribe(() => {
+				// somo code
+			})
+		});
+	}
+}
